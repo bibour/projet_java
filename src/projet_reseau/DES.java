7a6ca316709229b4494398e02ae54b594401d428
Damien Carrie <damien0081@gmail.com>
2024-10-11 12:17:22 +0200

    genere clef

--- src/projet_reseau/DES.java
+++ src/projet_reseau/DES.java
@@ -12,7 +12,7 @@ public class DES {
 	private static final int nb_ronde = 1;
 	private static final int[] tab_decalage = {1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1};
 	private static final int[] perm_initiale = {57,49,41,33,25,17,9,1,59,51,43,35,27,19,11,3,61,53,45,37,29,21,13,5,63,55,47,39,31,23,15,7,56,48,40,32,24,16,8,0,58,50,42,34,26,18,10,2,60,52,44,36,28,20,12,4,62,54,46,38,30,22,14,6};
-	private static final int[][] pc1 = {{56,48,40,32,24,16,8,0,5,0,57,49,41,33,25,17,9,1,58,50,42,34,26,18,10,2,59,51,43,35},{62,54,46,38,30,22,14,6,61,53,45,37,29,21,13,5,60,52,44,36,28,20,12,4,27,19,11,3}};
+	private static final int[]pc1 = {56,48,40,32,24,16,8,0,5,0,57,49,41,33,25,17,9,1,58,50,42,34,26,18,10,2,59,51,43,35,62,54,46,38,30,22,14,6,61,53,45,37,29,21,13,5,60,52,44,36,28,20,12,4,27,19,11,3};
 
 	private int[] PC2 = {13,16,10,23,0,4,2,27,14,5,20,9,22,18,11,3,25,7,15,6,26,19,12,1,40,51,30,36,46,54,29,39,50,44,32,47,43,48,38,55,33,52,45,41,49,35,28,31};
 	
@@ -35,7 +35,7 @@ public class DES {
 	
 	public int[] stringToBits(String message) {
 		byte[] bytes = message.getBytes();
-		int[] bit = new int[64];
+		int[] bit = new int[message.length()*8];
 		int i = 0;
 		for (byte b : bytes) {
 			int val = b;
@@ -83,7 +83,7 @@ public class DES {
 		int longueur = tab_permutation.length;
 		int[] perm = new int[longueur];
 		for (int i=0; i<longueur; i++) {
-			perm[i] = bloc[tab_permutation[i]];
+			perm[tab_permutation[i]] = bloc[i];
 		}
 		return perm;
 	}
@@ -91,9 +91,66 @@ public class DES {
 	public int[] invPermutation(int[] tab_permutation, int[] bloc) {
 		int longueur = tab_permutation.length;
 		int[] perm = new int[longueur];
-		
+		for (int i=0; i<longueur; i++) {
+			perm[i] = bloc[tab_permutation[i]];
+		}
+		return perm;
 	}
 	
+	public int[][] decoupage(int[] bloc, int tailleBlocs){
+		int longueur_decoupage = bloc.length/this.taille_bloc +1;
+		int[][] decoup = new int[longueur_decoupage][tailleBlocs];
+		for (int i=0; i<longueur_decoupage; i++) {
+			for (int j=0; j<tailleBlocs; j++) {
+				decoup[i][j] = bloc[i*tailleBlocs+j];
+			}
+		}
+		return decoup;
+	}
+	
+	public int[] recollage_bloc(int[][] blocs) {
+		int long_recollage_bloc = blocs[0].length * blocs.length;
+		int[] recollage = new int[long_recollage_bloc];
+		for (int i=0; i<blocs.length; i++) {
+			for (int j=0; j<blocs[1].length; j++){
+				recollage[i*blocs.length + j]= blocs[i][j];
+			}
+		}
+		return recollage;
+	}
+	
+	public int[] decalle_gauche(int[] bloc, int nbCran) {
+		int[] decalle = new int[bloc.length];
+		for (int i=nbCran; i<bloc.length-2; i++) {
+			decalle[i-nbCran] = bloc[i];
+		}
+		for (int i = 0; i<nbCran; i++) {
+			decalle[decalle.length-nbCran+i] = bloc[i];
+		}
+		return decalle;
+	}
+	
+	public int[] xor(int[] tab1, int[] tab2) {
+		int taille = tab1.length;
+		int[] tab_xor = new int[taille];
+		for (int i=0; i<taille; i++) {
+			if (tab1[i] + tab2[i] == 2) {
+				tab_xor[i] = 0;
+			}
+			else {
+				tab_xor[i] = tab1[i] + tab2[i];
+			}
+		}
+		return tab_xor;
+	}
+	
+	public void genereCle(int n) {
+		int[][] pc1 = decoupage(permutation(this.pc1, masterKey), 28);
+		for (int i=0; i<pc1.length; i++) {
+			pc1[i] = decalle_gauche(pc1[i], this.tab_decalage[nb_ronde]);
+		}
+		//Continuer en recollant les blocs et faire pc2
+	}
 	
 	public static void main(String[] args) {
 		DES d = new DES();
